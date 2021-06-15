85c96dcde328e6fe71fa09ec39f0288cd8a67d4d
Flavia964 <flavia.lima643@gmail.com>
2021-06-15 15:30:34 -0300

    Aula 1

---
+++ 1-variaveis.js
@@ -0,0 +1,8 @@
+const nome = "Flávia";
+console.log(nome);
+
+const nome = "Lais";
+const Sobrenome = "Silva"
+const Idade = 30;
+const anoDeNascimento = 1993;
+//Esse código não segue um padrão definido na declaração de variáveis.
